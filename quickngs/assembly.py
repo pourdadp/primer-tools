ee06a62b754b8a175fa43130ae0a60b73818726e
Pourdad Panahi <pourdadp@gmail.com>
2026-06-28 19:21:34 +0330

    Update assembly.py

--- quickngs/assembly.py
+++ quickngs/assembly.py
@@ -277,4 +277,242 @@ def debruijn_assemble(reads, k=3):
     if not reads:
         return ''
     graph = {}
-    for read in
+    for read in reads:
+        for i in range(len(read) - k + 1):
+            kmer = read[i:i+k]
+            prefix = kmer[:-1]
+            suffix = kmer[1:]
+            graph.setdefault(prefix, []).append(suffix)
+    if not graph:
+        return ''
+    start = list(graph.keys())[0]
+    contig = start
+    current = start
+    while current in graph and graph[current]:
+        next_node = graph[current].pop(0)
+        contig += next_node[-1]
+        current = next_node
+    return contig
+
+# ---------- Intelligent Clustering ----------
+def cluster_reads(reads, min_overlap=3, max_mismatch=3):
+    n = len(reads)
+    graph = {i: set() for i in range(n)}
+    for i in range(n):
+        for j in range(i+1, n):
+            ov = find_overlap(reads[i], reads[j], min_overlap, 50, max_mismatch)
+            if ov >= min_overlap:
+                graph[i].add(j)
+                graph[j].add(i)
+            else:
+                ov = find_overlap(reads[j], reads[i], min_overlap, 50, max_mismatch)
+                if ov >= min_overlap:
+                    graph[i].add(j)
+                    graph[j].add(i)
+    visited = set()
+    clusters = []
+    for i in range(n):
+        if i not in visited:
+            component = []
+            stack = [i]
+            while stack:
+                node = stack.pop()
+                if node not in visited:
+                    visited.add(node)
+                    component.append(node)
+                    stack.extend(graph[node] - visited)
+            clusters.append(component)
+    return clusters
+
+# ---------- Cluster Merging (Scaffolding) ----------
+def merge_clusters(contigs, min_overlap=10, max_mismatch=0):
+    if len(contigs) <= 1:
+        return contigs, {i: [i+1] for i in range(len(contigs))}
+    n = len(contigs)
+    merged = list(contigs)
+    merge_map = {i: [i] for i in range(n)}
+    changed = True
+    while changed:
+        changed = False
+        for i in range(len(merged)):
+            for j in range(i+1, len(merged)):
+                if not merged[i] or not merged[j]:
+                    continue
+                ov = find_overlap(merged[i], merged[j], min_overlap, 50, max_mismatch)
+                if ov >= min_overlap:
+                    new_contig = merged[i] + merged[j][ov:]
+                    merged[i] = new_contig
+                    merge_map[i].extend(merge_map.pop(j, []))
+                    merged[j] = ''
+                    changed = True
+                    break
+                ov = find_overlap(merged[j], merged[i], min_overlap, 50, max_mismatch)
+                if ov >= min_overlap:
+                    new_contig = merged[j] + merged[i][ov:]
+                    merged[j] = new_contig
+                    merge_map[j].extend(merge_map.pop(i, []))
+                    merged[i] = ''
+                    changed = True
+                    break
+                rc_i = reverse_complement(merged[i])
+                ov = find_overlap(rc_i, merged[j], min_overlap, 50, max_mismatch)
+                if ov >= min_overlap:
+                    new_contig = rc_i + merged[j][ov:]
+                    merged[i] = new_contig
+                    merge_map[i].extend(merge_map.pop(j, []))
+                    merged[j] = ''
+                    changed = True
+                    break
+                rc_j = reverse_complement(merged[j])
+                ov = find_overlap(rc_j, merged[i], min_overlap, 50, max_mismatch)
+                if ov >= min_overlap:
+                    new_contig = rc_j + merged[i][ov:]
+                    merged[j] = new_contig
+                    merge_map[j].extend(merge_map.pop(i, []))
+                    merged[i] = ''
+                    changed = True
+                    break
+            if changed:
+                break
+        merged = [c for c in merged if c]
+    final_merge_map = {}
+    new_idx = 0
+    for old_idx in range(len(contigs)):
+        for m_idx, m_list in merge_map.items():
+            if old_idx in m_list:
+                if m_idx not in final_merge_map:
+                    final_merge_map[m_idx] = []
+                final_merge_map[m_idx].append(old_idx + 1)
+                break
+    return merged, final_merge_map
+
+# ---------- Guided Assembly ----------
+def guided_assemble_fastq(r1_fastq, r2_fastq, ref_fasta, results_folder):
+    for tool in ['bwa', 'samtools', 'bcftools']:
+        if not is_tool_available(tool):
+            raise RuntimeError(f"{tool} is not installed. Please install it before running guided assembly.")
+    
+    sample = os.path.basename(r1_fastq).replace('_R1.fastq', '')
+    sam_file = os.path.join(results_folder, f"{sample}.sam")
+    bam_file = os.path.join(results_folder, f"{sample}.bam")
+    sorted_bam = os.path.join(results_folder, f"{sample}.sorted.bam")
+    
+    with open(sam_file, 'w') as out:
+        subprocess.run(['bwa', 'mem', '-M', '-R', f'@RG\\tID:{sample}\\tSM:{sample}', ref_fasta, r1_fastq, r2_fastq],
+                       stdout=out, stderr=subprocess.PIPE, check=True)
+    with open(bam_file, 'w') as out:
+        subprocess.run(['samtools', 'view', '-bS', sam_file], stdout=out, check=True)
+    subprocess.run(['samtools', 'sort', '-o', sorted_bam, bam_file], check=True)
+    subprocess.run(['samtools', 'index', sorted_bam], check=True)
+    
+    consensus_file = os.path.join(results_folder, f"{sample}_consensus.fa")
+    with open(consensus_file, 'w') as out:
+        p1 = subprocess.run(['samtools', 'mpileup', '-uf', ref_fasta, sorted_bam], capture_output=True, check=True)
+        p2 = subprocess.run(['bcftools', 'call', '-c'], input=p1.stdout, capture_output=True, check=True)
+        p3 = subprocess.run(['bcftools', 'consensus', '-f', ref_fasta], input=p2.stdout, stdout=out, check=True)
+    
+    with open(consensus_file) as f:
+        contig = f.read().strip()
+    
+    avg_depth, cov_20x = 0, 0
+    try:
+        depth_out = subprocess.run(['samtools', 'depth', sorted_bam], capture_output=True, text=True)
+        depths = [int(l.split()[2]) for l in depth_out.stdout.strip().split('\n') if l]
+        if depths:
+            avg_depth = sum(depths) / len(depths)
+            cov_20x = sum(1 for d in depths if d >= 20) / len(depths) * 100
+    except:
+        pass
+    
+    return {
+        'contig': contig, 'variants': [], 'avg_depth': avg_depth,
+        'coverage_20x': cov_20x,
+        'placements': [{'seq': contig, 'start': 0, 'end': len(contig)-1, 'mismatches': set()}]
+    }
+
+# ---------- Master Assembly Function ----------
+def assemble_reads_from_files(filepaths, mode='greedy', min_overlap=3, max_mismatch=3, k=3,
+                              orientation='auto', ref_fasta=None, results_folder=None,
+                              trim_low_quality=False, quality_threshold=20, window_size=5):
+    all_trim_info = []
+    
+    if ref_fasta:
+        reads, _ = parse_uploaded_files(filepaths, trim_low_quality, quality_threshold, window_size)
+        r1_path = os.path.join(results_folder, 'combined_R1.fastq')
+        r2_path = os.path.join(results_folder, 'combined_R2.fastq')
+        with open(r1_path, 'w') as f1, open(r2_path, 'w') as f2:
+            for i, seq in enumerate(reads):
+                qual = ''.join(chr(40+33) for _ in seq)
+                f1.write(f"@read{i}\n{seq}\n+\n{qual}\n")
+                f2.write(f"@read{i}_R2\n\n+\n\n")
+        return guided_assemble_fastq(r1_path, r2_path, ref_fasta, results_folder)
+
+    reads, all_trim_info = parse_uploaded_files(filepaths, trim_low_quality, quality_threshold, window_size)
+    clusters = cluster_reads(reads, min_overlap, max_mismatch)
+
+    if len(clusters) > 1:
+        contigs = []
+        cluster_read_indices = []
+        for indices in clusters:
+            clust_reads = [reads[i] for i in indices]
+            if mode == 'greedy':
+                assembly = greedy_assemble(clust_reads, min_overlap, 50, max_mismatch, orientation)
+                contig = assembly['contig']
+            elif mode == 'alignment':
+                contig = assemble_with_alignment(clust_reads, min_overlap, orientation)
+            elif mode == 'debruijn':
+                contig = debruijn_assemble(clust_reads, k)
+            else:
+                contig = ''
+            if contig:
+                contigs.append(contig)
+                cluster_read_indices.append(indices)
+
+        merged_contigs, merge_map = merge_clusters(contigs, min_overlap=min_overlap*2, max_mismatch=max_mismatch)
+
+        all_contigs = []
+        for i, contig in enumerate(merged_contigs):
+            contributing = merge_map.get(i, [i+1])
+            all_reads_used = []
+            for cluster_id in contributing:
+                orig_idx = cluster_id - 1
+                if orig_idx < len(cluster_read_indices):
+                    all_reads_used.extend(cluster_read_indices[orig_idx])
+            all_contigs.append({
+                'cluster_id': i + 1,
+                'contig': contig,
+                'reads_used': len(all_reads_used),
+                'read_names': [f"read_{r+1}" for r in sorted(set(all_reads_used))],
+                'length': len(contig),
+                'merged_from': contributing if len(contributing) > 1 else None
+            })
+
+        return {
+            'contig': all_contigs[0]['contig'] if all_contigs else '',
+            'all_contigs': all_contigs,
+            'total_clusters': len(all_contigs),
+            'initial_clusters': len(clusters),
+            'reads_count': len(reads),
+            'mode': mode,
+            'multi_cluster': True,
+            'trim_info': all_trim_info
+        }
+    else:
+        if mode == 'greedy':
+            res = greedy_assemble(reads, min_overlap, 50, max_mismatch, orientation)
+        elif mode == 'alignment':
+            contig = assemble_with_alignment(reads, min_overlap, orientation)
+            res = {'contig': contig, 'steps': [], 'placements': [], 'remaining': [], 'total': len(reads)}
+        elif mode == 'debruijn':
+            contig = debruijn_assemble(reads, k)
+            res = {'contig': contig, 'k': k, 'reads_count': len(reads)}
+        else:
+            res = {'error': 'Unknown mode'}
+        res['reads_count'] = len(reads)
+        res['multi_cluster'] = False
+        res['all_contigs'] = [{'cluster_id': 1, 'contig': res['contig'], 'reads_used': len(reads),
+                                'read_names': [f"read_{i+1}" for i in range(len(reads))],
+                                'length': len(res['contig']), 'merged_from': None}]
+        res['total_clusters'] = 1
+        res['trim_info'] = all_trim_info
+        return res
